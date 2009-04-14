afb338229e8a741b3456aab0fff2f9bda9043922
hjc0720@gmail.com <hjc0720@gmail.com@9ba966f2-28b8-11de-b89a-ab5ced7cf1a0>
2009-04-14 11:48:53 +0000

    add initial file

---
+++ record.cpp
@@ -0,0 +1,53 @@
+#include "record.h"
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+record::record()
+{
+	m_remark.clear();
+}
+
+record::~record()
+{
+}
+
+void record::initial(unsigned long date, long pay, unsigned char type,long income, const string& remark)
+{
+	m_date = date;
+	m_pay = pay;
+	m_type = type;
+	m_remark = remark;
+	m_income = income;
+}
+
+void record::save(ofstream& outFile)
+{
+	int nSize = m_remark.size();
+	outFile.write((char*)&m_date,sizeof(unsigned long));
+	outFile.write((char*)&m_income,sizeof(long));
+	outFile.write((char*)&m_pay,sizeof(long));
+	outFile.write((char*)&m_type,sizeof(unsigned char));
+	outFile.write((char*)&nSize,sizeof(int));
+	outFile << m_remark;
+}
+
+void record::load(ifstream& inFile)
+{
+	inFile.read((char*)&m_date,sizeof(unsigned long));
+	inFile.read((char*)&m_income,sizeof(long));
+	inFile.read((char*)&m_pay,sizeof(long));
+	inFile.read((char*)&m_type,sizeof(unsigned char));
+	int stringSize;
+	inFile.read((char*)&stringSize,sizeof(int));
+	char* readString = new char[stringSize + 1];
+	inFile.read(readString,stringSize);
+//	readString[stringSize] = '\0';
+	m_remark.clear();
+	m_remark.append(readString,stringSize);
+}
+
+void record::print()
+{
+	cout << m_remark<<endl;
+}
---
+++ record.h
@@ -0,0 +1,33 @@
+#ifndef RECORD_H_HJC20090409
+#define RECORD_H_HJC20090409
+#include <string>
+using namespace std;
+
+enum payIncomeType{
+	GetFromWife,
+	GetFromCard,
+	Salary,
+	PayForAccessory,
+	PayForEat,
+	PayForRestaurant,
+	PayForClothes,
+
+};
+
+class record
+{
+	private:
+		unsigned long m_date;
+		long m_pay;
+		long m_income;
+		unsigned char m_type;
+		string m_remark;
+	public:
+		record();
+		~record();
+		void initial(unsigned long date, long pay, unsigned char type, long income, const string& remark);
+		void save(ofstream& outFile);
+		void load(ifstream& inFile);
+		void print();
+};
+#endif
