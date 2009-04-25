a62d484ae4e35cb0e3f901d1dcaf992e72587aa8
hjc0720@gmail.com <hjc0720@gmail.com@9ba966f2-28b8-11de-b89a-ab5ced7cf1a0>
2009-04-25 11:58:38 +0000

    add menu class

--- accountBook.cpp
+++ accountBook.cpp
@@ -3,6 +3,21 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+void selectMainMenu(int select)
+{
+	int subSelect = -1;
+	do{
+		switch(select)
+		{
+			case 1:
+				subSelect = showRecord();
+				break;
+			default:
+				subSelect = 0;
+				break;
+		}
+	}while(subSelect != 0);
+}
 int main()
 {
 	account bank("银行卡");
@@ -16,8 +31,12 @@ int main()
 	}
 	cout << bank.getName() << "\t余额：" <<bank.getLeft() << endl;
 	cout << cash.getName() << "\t余额：" <<cash.getLeft() << endl;
-
 	int select = showMainMenu();
+	while(select != 0)
+	{
+		selectMainMenu(select);
+		select = showMainMenu();
+	}
 	return 1;
 }
 
--- menu.h
+++ menu.h
@@ -1,8 +1,30 @@
 #ifndef MENUTEST_HJC_20090424
 #define MENUTEST_HJC_20090424
-
+#include <string>
+#include <vector>
+using namespace std;
+class CMenu;
+typedef void (*menuFun)(CMenu**);
 int showMainMenu();
 
 int showRecord();
+
+class CMenu
+{
+	public:
+		CMenu(const string& menuName,menuFun pMenuFun = NULL);
+		~CMenu();
+		void addSubMenu(CMenu* subMenu);
+		void delSubMenu(string& subMenuName);
+		void delSubMenu(int nIndex);
+		int showSubMenu();
+		void selectSubMenu(int select, CMenu** nowMenu);
+		const string& getString()const {return m_menuName;};
+		menuFun m_fun;
+	private:
+		string m_menuName;
+		vector<CMenu*> m_subMenu;
+};
+
 
 #endif
