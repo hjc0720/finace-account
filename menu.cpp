9ee9583dc1e9e667350e55fd77dc6498871effa7
hjc0720@gmail.com <hjc0720@gmail.com@9ba966f2-28b8-11de-b89a-ab5ced7cf1a0>
2009-04-25 09:15:51 +0000

    main menu

--- menu.cpp
+++ menu.cpp
@@ -10,7 +10,24 @@ int showMainMenu()
 		cout << "2、添加记录"<<endl;
 		cout << "请选择菜单:";
 		cin >> i;
-	}while(i < 1 && i > 2);
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+	}while(i < 1 || i > 2);
 	return i;
 }
 
+int showRecord()
+{
+	int i = 0;
+	do{
+		cout << "1、查看当月全部记录";
+		cout << "2、查看指定时间记录";
+		cout << "3、查看某天记录";
+		cout << "4、设定时间";
+		cout << "请选择菜单:";
+		cin >> i;
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+	}while(i < 1 || i >4);
+	return i;
+} 
