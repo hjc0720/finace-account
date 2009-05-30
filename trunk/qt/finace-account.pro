f3c3aed59faaadd05b64ee40dba26a06b13bdbb6
hjc0720@gmail.com <hjc0720@gmail.com@9ba966f2-28b8-11de-b89a-ab5ced7cf1a0>
2009-05-30 13:55:03 +0000

    add qt

---
+++ trunk/qt/finace-account.pro
@@ -0,0 +1,16 @@
+# -------------------------------------------------
+# Project created by QtCreator 2009-05-13T07:23:53
+# -------------------------------------------------
+TARGET = finace-account
+TEMPLATE = app
+SOURCES += main.cpp \
+    mainwindow.cpp \
+    ../account.cpp \
+    ../record.cpp \
+    ../basefunction.cpp
+HEADERS += mainwindow.h \
+    ../record.h \
+    ../basefunction.h \
+    ../account.h
+FORMS += 
+OTHER_FILES += 
---
+++ trunk/qt/mainwindow.cpp
@@ -0,0 +1,64 @@
+#include "mainwindow.h"
+#include <iostream>
+#include <QAction>
+#include <QMenuBar>
+#include <QTableWidget>
+using namespace std;
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), fileName("accountRecord")
+{
+    table = new QTableWidget(10,6,this);
+    setCentralWidget(table);
+    createActions();
+    createMenu();
+}
+
+void MainWindow::createActions()
+{
+    action_save = new QAction(tr("&Save"),this);
+    action_save->setShortcut(tr("Ctrl+S"));
+   connect(action_save,SIGNAL(triggered()),this,SLOT(save()));
+
+    action_exit = new QAction(tr("&Quit"),this);
+    action_exit->setShortcut(tr("Ctrl+Q"));
+    connect(action_exit,SIGNAL(triggered()),this,SLOT(close()));
+}
+
+void MainWindow::createMenu()
+{
+    fileMenu = menuBar()->addMenu(tr("&File"));
+    fileMenu->addAction(action_save);
+    fileMenu->addAction(action_exit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete fileMenu;
+    delete action_save;
+    delete action_exit;
+}
+
+void MainWindow::initial()
+{
+    ifstream ifile;
+    ifile.open(fileName.c_str());
+    if(ifile)
+    {
+        m_bank.load(ifile);
+        m_cash.load(ifile);
+    }
+    ifile.close();
+}
+
+void MainWindow::save()
+{
+    ofstream ofile;
+    ofile.open(fileName.c_str());
+    if(ofile)
+    {
+        m_bank.save(ofile);
+        m_cash.save(ofile);
+    }
+    ofile.close();
+}
